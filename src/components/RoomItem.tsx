846a3e9c29074d6d867a41d5823189a924a3c8b7
Akshit Garg <garg.akshit@gmail.com>
2022-10-19 19:15:53 +0530

    refactor: cleanup code
    Signed-off-by: Akshit Garg <garg.akshit@gmail.com>

--- src/components/RoomItem.tsx
+++ src/components/RoomItem.tsx
@@ -2,20 +2,19 @@ import { Component, JSX } from "solid-js";
 import Emoji from "./Emoji";
 
 const RoomItem: Component<{
-    backdrop: string;
-    class?: string;
-    emoji?: string;
-    name?: string;
-    onClick?: () => void;
+  backdrop: string;
+  class?: string;
+  emoji?: string;
+  name?: string;
+  onClick?: () => void;
 }> = (props) => {
-    return (
-        <div class="flex flex-row align-center w-72 mb-4 mt-2">
-            <Emoji emoji={props.emoji} backdrop={props.backdrop} />
-            <div class="w-5" aria-hidden></div>
-            <p class="flex-3 text-xl self-center font-light">{props.name}</p>
-        </div>
-
-    )
+  return (
+    <div class="flex flex-row align-center w-72 mb-4 mt-2">
+      <Emoji emoji={props.emoji} backdrop={props.backdrop} />
+      <div class="w-5" aria-hidden></div>
+      <p class="flex-3 text-xl self-center font-light">{props.name}</p>
+    </div>
+  );
 };
 
 export default RoomItem;
--- src/pages/Discover.tsx
+++ src/pages/Discover.tsx
@@ -1,44 +1,29 @@
 import { Component } from "solid-js";
-import { SexyButton } from "../components/Button";
+
 import RoomItem from "../components/RoomItem";
 
-const fadeVariant = {
-    hidden: { opacity: 0 },
-    visible: { opacity: 1, transition: { duration: 1, ease: "easeInOut" } },
+const DiscoverPage: Component = () => {
+  return (
+    <div class="px-20 pt-20">
+      <h1 class="font-bold text-5xl heading">Discovering...</h1>
+      <p class="mt-2 mb-14">Finding nearby shares</p>
+      {Rooms.map((room) => (
+        <RoomItem
+          emoji={room.emoji}
+          name={room.name}
+          backdrop={room.backdrop}
+        />
+      ))}
+    </div>
+  );
 };
 
-// TODO: make go home button responsive
-
-const DiscoverPage: Component = () => {
-
-    return (<div class="ml-16 mr-16 mt-24 h-100">
-        <div class="flex flex-row justify-between header">
-            <h1 class="font-bold text-5xl heading">Discovering...</h1>
-            <div class="mr-16 w-52">
-                <SexyButton href="/">Go Home</SexyButton>
-            </div>
-        </div>
-        <div class="flex flex-col items-left ">
-            <div class="h-2" aria-hidden></div>
-            <p>Finding nearby shares</p>
-            <div class="h-14" aria-hidden />
-            {Rooms.map((room) => (
-                <RoomItem emoji={room.emoji} name={room.name} backdrop={room.backdrop} />
-            ))}
-        </div>
-    </div>
-    )
-}
-
-
-
 const Rooms = [
-    { name: "Recoiled Goblins", emoji: "🍑", backdrop: "#FC7A57" },
-    { name: "Oxidising Yeast", emoji: "🍆", backdrop: "#EA7AF4" },
-    { name: "Moonlight Rusk", emoji: "🍌", backdrop: "#FEEFA7" },
-    { name: "Sinister Shelf", emoji: "🍟", backdrop: "#DB5461" },
-    { name: "Squidgy Sausage", emoji: "🍔", backdrop: "#F7B267" },
+  { name: "Recoiled Goblins", emoji: "🍑", backdrop: "#FC7A57" },
+  { name: "Oxidising Yeast", emoji: "🍆", backdrop: "#EA7AF4" },
+  { name: "Moonlight Rusk", emoji: "🍌", backdrop: "#FEEFA7" },
+  { name: "Sinister Shelf", emoji: "🍟", backdrop: "#DB5461" },
+  { name: "Squidgy Sausage", emoji: "🍔", backdrop: "#F7B267" },
 ];
-
 
 export default DiscoverPage;
