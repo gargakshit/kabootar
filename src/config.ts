f4d0ddd147c833270d9834eabafc0e6daea76bb1
Akshit Garg <garg.akshit@gmail.com>
2023-02-18 17:03:13 +0530

    Use environment variables for base deployment URLs
    Signed-off-by: Akshit Garg <garg.akshit@gmail.com>

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ export const iceServers: RTCConfiguration = {
   iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
 };
 
-export const secure = true;
-export const baseURL = "signaling.staging.kabootar.potatokitty.me";
+export const baseURL = import.meta.env.VITE_BASE_URL ?? "localhost:5000";
+export const secure = !baseURL.startsWith("localhost");
 export const httpScheme = secure ? "https://" : "http://";
 export const wsScheme = secure ? "wss://" : "ws://";
